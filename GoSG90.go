79a20f0252d43fcc924eb82767b92dcc13f3f6f3
EBA5 <eloibarti@gmail.com>
2020-07-05 21:17:52 +0200

    Continuing implementation

--- GoSG90.go
+++ GoSG90.go
@@ -5,14 +5,31 @@ import (
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
+/*
+type sg90servo interface {
+	new(GPIOindex uint8) SG90
+	init() (err error)
+	getCurrentLocation() (currentAngle float64, err error)
+	setTargetLocation(targetAngle float64) (err error)
+	movePlus() (err error)
+	moveMinus() (err error)
+	angleRange() float64
+	dutyRange() float64
+	toAngle(dutyLength, cycleLength uint32) (angle float64)
+	toDuty(targetAngle float64) (dutyLength, cycleLength uint32, err error)
+}
+*/
+
+type mockablePin struct {
+}
 
 type SG90 struct {
-	Pin rpio.Pin
-	OperatingFreq uint32
+	Pin                rpio.Pinner
+	OperatingFreq      uint32
 	MaxDutyCycleFactor float64
 	MinDutyCycleFactor float64
-	MinAngle float64
-	MaxAngle float64
+	MinAngle           float64
+	MaxAngle           float64
 }
 
 // Pin.Freq -> Param freq should be in range 4688Hz - 19.2MHz to prevent unexpected behavior
@@ -27,13 +44,29 @@ type SG90 struct {
 //  pin.DutyCycle(1, 4)
 //  pin.Freq(38000*4)
 
+// Creates a new SG90 servo using the desired GPIO pin number.
+// This module uses PWM. A raspberry pi has 2 PWM channels:
+//   channel 1 (pwm0) for pins 12, 18, 40
+//   channel 2 (pwm1) for pins 13, 19, 41, 45.
 func New(GPIOindex uint8) SG90 {
 	// raspberry pi 4
-	return SG90 {
-		Pin: rpio.Pin(GPIOindex),
+	validPins := map[uint8]bool{
+		12: true,
+		13: true,
+		18: true,
+		19: true,
+		40: true,
+		41: true,
+		45: true,
+	}
+	if _, exists := validPins[GPIOindex]; false == exists {
+		panic(errors.New("invalid GPIO number"))
+	}
+	return SG90{
+		Pin:           rpio.Pin(GPIOindex),
 		OperatingFreq: 50,
-		MinAngle: -90.0,
-		MaxAngle:  90.0,
+		MinAngle:      -90.0,
+		MaxAngle:      90.0,
 	}
 }
 
@@ -50,7 +83,6 @@ func (servo *SG90) GetCurrentLocation() (currentAngle float64, err error) {
 	}
 	return
 }
-
 
 func (servo *SG90) SetTargetLocation(targetAngle float64) (err error) {
 	if targetAngle < servo.MinAngle || targetAngle > servo.MaxAngle {
@@ -80,25 +112,24 @@ func (servo *SG90) MoveMinus() (err error) {
 	return
 }
 
-
 func (servo *SG90) angleRange() float64 {
-	return servo.MaxAngle-servo.MinAngle
+	return servo.MaxAngle - servo.MinAngle
 }
 
 func (servo *SG90) dutyRange() float64 {
-	return servo.MaxDutyCycleFactor-servo.MinDutyCycleFactor
+	return servo.MaxDutyCycleFactor - servo.MinDutyCycleFactor
 }
 
 func (servo *SG90) toAngle(dutyLength, cycleLength uint32) (angle float64) {
-	currentDutyCycle := float64(dutyLength)/float64(cycleLength)
-	angle = servo.MinAngle + (currentDutyCycle - servo.MinDutyCycleFactor) * servo.angleRange() / servo.dutyRange()
+	currentDutyCycle := float64(dutyLength) / float64(cycleLength)
+	angle = servo.MinAngle + (currentDutyCycle-servo.MinDutyCycleFactor)*servo.angleRange()/servo.dutyRange()
 	return
 }
 
 func (servo *SG90) toDuty(targetAngle float64) (dutyLength, cycleLength uint32, err error) {
 	dutyLength, cycleLength, err = servo.Pin.GetDutyCycle()
 	if nil == err {
-		targetDutyCycle := servo.MinDutyCycleFactor + (targetAngle - servo.MinAngle) * servo.dutyRange() / servo.angleRange()
+		targetDutyCycle := servo.MinDutyCycleFactor + (targetAngle-servo.MinAngle)*servo.dutyRange()/servo.angleRange()
 		dutyLength = uint32(targetDutyCycle * float64(cycleLength))
 	}
 	return
--- go.mod
+++ go.mod
@@ -2,6 +2,11 @@ module GoSG90
 
 go 1.14
 
-require github.com/stianeikeland/go-rpio/v4 v4.4.0
+require (
+	github.com/golang/mock v1.4.3 // indirect
+	github.com/stianeikeland/go-rpio/v4 v4.4.0
+	golang.org/x/mod v0.3.0 // indirect
+	golang.org/x/tools v0.0.0-20200702044944-0cc1aa72b347 // indirect
+)
 
 replace github.com/stianeikeland/go-rpio/v4 v4.4.0 => github.com/ebarti/go-rpio/v4 v4.4.1
